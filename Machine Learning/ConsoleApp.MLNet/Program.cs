a35def90002e85e662b662642b2246a1ade60c16
Angel Vargas <angelsk814@gmail.com>
2022-02-03 23:44:43 -0500

    Construyendo un modelo Machine Learning para Sentimiento de Analisis ML.NET [4 of 8]
    Ejemplo en Aplicacion de consola .Net5

---
+++ Machine Learning/ConsoleApp.MLNet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ConsoleApp_MLNetML.Model;
+
+
+namespace ConsoleApp.MLNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Add input data
+            var input = new ModelInput();
+            input.SentimentText = "Tat is cool"; //or Tat is cool
+            
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            var res = result.Prediction;
+
+            //El resultado verdadero (1) significa que es toxico y falso (0) que no es toxico
+            if (res.Equals("1")) Console.WriteLine("Es toxico");
+            else Console.WriteLine("No es toxico");
+        }
+    }
+}
