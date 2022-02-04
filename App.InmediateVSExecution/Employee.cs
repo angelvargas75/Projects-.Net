7c44321b2717122afcf799c353a8a564ad649455
Angel Vargas <angelsk814@gmail.com>
2022-02-04 01:04:50 -0500

    Ejemplo sobre la diferencia entre Immediate vs Deferred Query Execution in LINQ
    Fuente:  https://www.c-sharpcorner.com/UploadFile/a20beb/immediate-vs-deferred-query-execution-in-linq/

---
+++ App.InmediateVSExecution/Employee.cs
@@ -0,0 +1,9 @@
+namespace App.InmediateVSExecution {
+public class Employee  
+    {  
+        public int Emp_ID { get; set; }  
+        public string Name { get; set; }  
+        //public string Email { get; set; }  
+        //public string Country { get; set; }  
+    }  
+}
---
+++ App.InmediateVSExecution/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace App.InmediateVSExecution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodo2();
+        }
+
+       
+        public static void Metodo1()
+        {
+            var list = new List<Employee>(new Employee[]{
+                new Employee{ Emp_ID=1, Name="A"},
+                new Employee{ Emp_ID=2, Name="B"},
+                new Employee{ Emp_ID=3, Name="C"},
+                new Employee{ Emp_ID=4, Name="D"},
+                new Employee{ Emp_ID=5, Name="E"}
+            });
+            var res = from r in list select new { r.Emp_ID, r.Name };
+            list.Add(new Employee { Emp_ID = 8, Name = "H" });
+            list.Add(new Employee { Emp_ID = 9, Name = "I" });
+            foreach (var item in res)
+            {
+                System.Console.WriteLine(item.Emp_ID + " - " + item.Name);
+            }
+        }
+
+        public static void Metodo2()
+        {
+            var list = new List<Employee>(new Employee[]{
+                new Employee{ Emp_ID=1, Name="A"},
+                new Employee{ Emp_ID=2, Name="B"},
+                new Employee{ Emp_ID=3, Name="C"},
+                new Employee{ Emp_ID=4, Name="D"},
+                new Employee{ Emp_ID=5, Name="E"}
+            });
+            var res = from r in list.ToList() select new { r.Emp_ID, r.Name };
+            list.Add(new Employee { Emp_ID = 8, Name = "H" });
+            list.Add(new Employee { Emp_ID = 9, Name = "I" });
+            foreach (var item in res)
+            {
+                System.Console.WriteLine(item.Emp_ID + " - " + item.Name);
+            }
+        }
+    }
+}
