613221d5e654f1777742a6253e546dd686046554
Angel Vargas <angelsk814@gmail.com>
2022-04-12 21:54:27 -0500

    Alojamiento de un servicio WCF en el servidor IIS
    Fuente:  https://www.c-sharpcorner.com/UploadFile/b95541/iis-hosting-of-wcf-service/

---
+++ WCF/CarService/CarService/IProduct.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+
+namespace CarService
+{
+    [ServiceContract]
+    interface IProduct
+    {
+        [OperationContract]
+        string GetProductName();
+
+        [OperationContract]
+        string GetProductQty();
+
+        [OperationContract]
+        string GetCategoryName();
+    }
+}
---
+++ WCF/CarService/CarService/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarService
+{
+    class ProductService : IProduct
+    {
+        public string GetCategoryName()
+        {
+            return "Toyota GT86";
+        }
+
+        public string GetProductName()
+        {
+            return "Sea gray";
+        }
+
+        public string GetProductQty()
+        {
+            return "1";
+        }
+    }
+}
